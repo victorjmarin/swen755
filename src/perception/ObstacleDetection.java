99d0c3229f77142c38d0d6d0ab4068c082e4ec2c
Victor J. Marin <vxm4964@rit.edu>
2017-10-03 14:11:39 -0400

    Change HearbeatSender to ObstacleDetection

---
+++ src/perception/ObstacleDetection.java
@@ -0,0 +1,48 @@
+package perception;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.ThreadLocalRandom;
+
+import perception.object.EnvObject;
+import systemmgmt.health.HeartbeatSender;
+
+public class ObstacleDetection implements IObstacleDetection {
+
+    private static final int PROCESS_NAME = 3;
+    private final HeartbeatSender _heartbeat;
+
+    public ObstacleDetection(final String heartbeatFilename, final int pid) {
+	_heartbeat = new HeartbeatSender(heartbeatFilename, pid, PROCESS_NAME);
+    }
+
+    public void run() {
+	while (true) {
+	    // Send heartbeat - we are alive!
+	    _heartbeat.sendHeartbeat();
+
+	    // Send new values to the actuators
+	    detectObstacles(new HashSet<EnvObject>());
+
+	    try {
+		Thread.sleep(500); // Pretend we are doing a long computation
+	    } catch (final InterruptedException e) {
+	    }
+	}
+    }
+
+    @Override
+    public Set<EnvObject> detectObstacles(final Set<EnvObject> objects) {
+	final long currTime = System.currentTimeMillis();
+	System.out.println("Detecting obstacles " + currTime);
+	final int noise = ThreadLocalRandom.current().nextInt(0, 20);
+	System.out.println("Performing important division: " + currTime / noise);
+	return null;
+    }
+
+    @Override
+    public double getCollisionForce() {
+	return 0;
+    }
+
+}
--- src/systemmgmt/Boot.java
+++ src/systemmgmt/Boot.java
@@ -7,76 +7,71 @@ import java.net.URLClassLoader;
 import java.util.ArrayList;
 
 import controller.Controller;
-import decision.Decision;
+import perception.ObstacleDetection;
 import systemmgmt.health.HeartbeatReceiver;
 
 public class Boot {
 
-	public static final ArrayList<Process> processes = new ArrayList<Process>();
+    public static final ArrayList<Process> processes = new ArrayList<Process>();
 
-	/**
-	 * 
-	 * @param args
-	 *            the processName to start or nothing to run main initialization
-	 * @throws InterruptedException
-	 */
-	public static void main(final String[] args) throws InterruptedException {
-		try {
-			final URLClassLoader url = (URLClassLoader) Thread.currentThread()
-					.getContextClassLoader();
-			String jarPath = url.getURLs()[0].toString();
-			final int fileGarbage = jarPath.indexOf('/');
-			jarPath = jarPath.substring(fileGarbage, jarPath.length());
+    /**
+     * @param args
+     *            the processName to start or nothing to run main initialization
+     * @throws InterruptedException
+     */
+    public static void main(final String[] args) throws InterruptedException {
+	try {
+	    final URLClassLoader url = (URLClassLoader) Thread.currentThread().getContextClassLoader();
+	    String jarPath = url.getURLs()[0].toString();
+	    final int fileGarbage = jarPath.indexOf('/');
+	    jarPath = jarPath.substring(fileGarbage, jarPath.length());
 
-			// First, check if this was the normal boot
-			if (args.length == 0) {
-				// Start all the processes
-				for (int i = 1; i < 4; ++i) {
-					final ProcessBuilder pb = new ProcessBuilder("java", "-jar",
-							jarPath, "" + i);
-					pb.redirectOutput(Redirect.INHERIT);
-					pb.redirectError(Redirect.INHERIT);
-					processes.add(pb.start());
-				}
-				Runtime.getRuntime().addShutdownHook(new Thread() {
-					@Override
-					public void run() {
-						for (final Process p : processes) {
-							p.destroy();
-						}
-					}
-				});
-				while (true)
-					;
-			}
-
-			// Otherwise, start the specific process
-			final String heartbeatFilename = jarPath.substring(0,
-					jarPath.length() - 7) + "heartbeat_communication";
-			{
-				final File file = new File(heartbeatFilename);
-				if (file.exists()) {
-					file.delete();
-				}
-			}
-
-			final int processName = Integer.parseInt(args[0]);
-
-			switch (processName) {
-			case 1:
-				new Controller(heartbeatFilename, 1).run();
-				break;
-			case 2:
-				new HeartbeatReceiver(heartbeatFilename).run();
-				break;
-			case 3:
-				new Decision(heartbeatFilename, 3).run();
-				break;
-			}
-
-		} catch (final IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+	    // First, check if this was the normal boot
+	    if (args.length == 0) {
+		// Start all the processes
+		for (int i = 1; i < 4; ++i) {
+		    final ProcessBuilder pb = new ProcessBuilder("java", "-jar", jarPath, "" + i);
+		    pb.redirectOutput(Redirect.INHERIT);
+		    pb.redirectError(Redirect.INHERIT);
+		    processes.add(pb.start());
 		}
+		Runtime.getRuntime().addShutdownHook(new Thread() {
+		    @Override
+		    public void run() {
+			for (final Process p : processes) {
+			    p.destroy();
+			}
+		    }
+		});
+		while (true)
+		    ;
+	    }
+
+	    // Otherwise, start the specific process
+	    final String heartbeatFilename = jarPath.substring(0, jarPath.length() - 7) + "heartbeat_communication";
+	    {
+		final File file = new File(heartbeatFilename);
+		if (file.exists()) {
+		    file.delete();
+		}
+	    }
+
+	    final int processName = Integer.parseInt(args[0]);
+
+	    switch (processName) {
+	    case 1:
+		new Controller(heartbeatFilename, 1).run();
+		break;
+	    case 2:
+		new HeartbeatReceiver(heartbeatFilename).run();
+		break;
+	    case 3:
+		new ObstacleDetection(heartbeatFilename, 3).run();
+		break;
+	    }
+
+	} catch (final IOException e) {
+	    e.printStackTrace();
 	}
+    }
 }
